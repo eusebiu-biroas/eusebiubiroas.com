8278581d0d7e1ba0f5b543a9560bec8837bc4eab
Eusebiu Biroas <eusebiu.biroas@stayfriends.com>
2019-02-13 21:28:52 +0200

    update resume - February 2019

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-IDRViewer.config = {"pagecount":3,"title":"","author":"Eusebiu Biroas","fileName":"Eusebiu Biroas - Lead Software Engineer","bounds":[[909,1286],[909,1286],[909,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
+IDRViewer.config = {"pagecount":3,"title":"","author":"Eusebiu Biroas","fileName":"Eusebiu Biroas - Lead Software Engineer | AWS Cloud Solutions Architect","bounds":[[909,1286],[909,1286],[909,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
